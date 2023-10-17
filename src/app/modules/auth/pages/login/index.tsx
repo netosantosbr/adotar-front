52519a37f27aee8f30560f94ebcf5dcb313ef081
João Paulo <jpcchaves@outlook.com>
2023-10-16 22:29:27 -0300

    feat: add login base validation

--- src/app/modules/auth/pages/login/index.tsx
+++ src/app/modules/auth/pages/login/index.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 import LoginView from "./view";
+import * as Yup from "yup";
+import { useFormik } from "formik";
 
 const Login = () => {
+  const validation = useFormik({
+    enableReinitialize: true,
+    initialValues: {
+      usernameOrEmail: "",
+      password: "",
+    },
+    validationSchema: Yup.object().shape({
+      usernameOrEmail: Yup.string()
+        .email("Informe um email válido")
+        .required("O campo é obrigatório"),
+      password: Yup.string().required("O campo é obrigatório"),
+    }),
+    onSubmit: (values) => console.log(values),
+  });
+
   return <LoginView />;
 };
 
