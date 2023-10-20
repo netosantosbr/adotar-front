7342e74a2fff74c0fb17fa450ab26628ab1016c4
João Paulo <jpcchaves@outlook.com>
2023-10-19 23:38:53 -0300

    feat: add user model

---
+++ src/app/modules/auth/models/UserModel.ts
@@ -0,0 +1,13 @@
+export interface UserModel {
+  id: number;
+  firstName: string;
+  lastName: string;
+  name: string;
+  username: string;
+  email: string;
+  createdAt: string;
+  updatedAt: string;
+  deletedAt: string;
+  active: boolean;
+  admin: boolean;
+}
