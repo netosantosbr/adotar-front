02167f273d43ca2e5845e7e0cd04266789db351f
jpcchaves <joao.chaves@liferay.com>
2023-10-20 13:00:24 -0300

    refactor: improvments in password input

--- src/app/modules/auth/components/passswordInput/index.tsx
+++ src/app/modules/auth/components/passswordInput/index.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import { WarningIcon } from "@chakra-ui/icons";
 import {
   FormControl,
+  FormErrorMessage,
   FormLabel,
   IconButton,
   Input,
@@ -9,27 +10,42 @@ import {
   InputRightElement,
   useDisclosure,
 } from "@chakra-ui/react";
+import React, { useCallback } from "react";
 import { HiEye, HiEyeOff } from "react-icons/hi";
 
 interface IProps extends InputProps {
   inputLabel?: string;
   inputName?: string;
+  isInvalid?: boolean;
+  isRequired: boolean;
+  handleChange: () => void;
+  handleBlur: () => void;
+  inputValue: string;
+  inputErrorMessage?: string;
+  inputIdentifier: string;
 }
 
 export const PasswordInput: React.FC<IProps> = ({
   inputLabel = "Senha",
   inputName = "password",
-  ...props
+  isInvalid,
+  isRequired,
+  handleChange,
+  handleBlur,
+  inputValue,
+  inputErrorMessage,
+  inputIdentifier,
+  ...rest
 }) => {
   const { isOpen, onToggle } = useDisclosure();
 
-  const onClickReveal = () => {
+  const onClickReveal = useCallback(() => {
     onToggle();
-  };
+  }, [isOpen]);
 
   return (
-    <FormControl>
-      <FormLabel htmlFor='password'>{inputLabel}</FormLabel>
+    <FormControl isInvalid>
+      <FormLabel htmlFor={inputIdentifier}>{inputLabel}</FormLabel>
       <InputGroup>
         <InputRightElement>
           <IconButton
@@ -37,16 +53,27 @@ export const PasswordInput: React.FC<IProps> = ({
             aria-label={isOpen ? "Mask password" : "Reveal password"}
             icon={isOpen ? <HiEyeOff /> : <HiEye />}
             onClick={onClickReveal}
+            mr={isInvalid ? "12" : ""}
           />
         </InputRightElement>
         <Input
-          id={inputName}
-          name={inputName}
-          type={isOpen ? "text" : "password"}
-          autoComplete='current-password'
-          {...props}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          name={inputIdentifier}
+          id={inputIdentifier}
+          value={inputValue}
+          type='text'
+          {...rest}
         />
+        {isInvalid && isRequired && (
+          <InputRightElement>
+            <WarningIcon color='red.600' />
+          </InputRightElement>
+        )}
       </InputGroup>
+      {isInvalid && isRequired && (
+        <FormErrorMessage>{inputErrorMessage}</FormErrorMessage>
+      )}
     </FormControl>
   );
 };
