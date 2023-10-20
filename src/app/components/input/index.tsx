7559fb080528136c6cee5c8b049f88ff3af37b14
jpcchaves <joao.chaves@liferay.com>
2023-10-20 12:59:51 -0300

    feat: create new global input component to reuse along the project

---
+++ src/app/components/input/index.tsx
@@ -0,0 +1,62 @@
+import { WarningIcon } from "@chakra-ui/icons";
+import {
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputProps,
+  InputRightElement,
+} from "@chakra-ui/react";
+import React from "react";
+
+interface IProps extends InputProps {
+  inputLabel: string;
+  isInvalid?: boolean;
+  isRequired: boolean;
+  handleChange: () => void;
+  handleBlur: () => void;
+  inputValue: string;
+  inputErrorMessage?: string;
+  inputIdentifier: string;
+}
+
+const InputComponent: React.FC<IProps> = ({
+  inputLabel,
+  isInvalid = false,
+  inputErrorMessage,
+  handleChange,
+  handleBlur,
+  inputValue,
+  inputIdentifier,
+  isRequired,
+  ...rest
+}) => {
+  return (
+    <FormControl isInvalid>
+      <FormLabel htmlFor={inputIdentifier}>{inputLabel}</FormLabel>
+      <InputGroup>
+        <Input
+          onChange={handleChange}
+          onBlur={handleBlur}
+          name={inputIdentifier}
+          id={inputIdentifier}
+          value={inputValue}
+          type='text'
+          {...rest}
+        />
+        {isInvalid && isRequired && (
+          <InputRightElement>
+            <WarningIcon color='red.600' />
+          </InputRightElement>
+        )}
+      </InputGroup>
+
+      {isInvalid && isRequired && (
+        <FormErrorMessage>{inputErrorMessage}</FormErrorMessage>
+      )}
+    </FormControl>
+  );
+};
+
+export default InputComponent;
