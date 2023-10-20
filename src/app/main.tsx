5699c9b7ff57388264ffca091a427e91a94663b9
João Paulo <jpcchaves@outlook.com>
2023-10-19 21:49:23 -0300

    refactor: adjusted import of theme config

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
-import { theme } from "./theme";
 import Router from "./routes";
+import theme from "./theme/theme";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
