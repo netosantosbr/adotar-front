d17f73b3a9a00e63497245d637a54a4c73b0d078
João Paulo <jpcchaves@outlook.com>
2023-10-22 11:01:11 -0300

    feat: add redux provider

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import Router from "./routes";
 import theme from "./theme/theme";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <Router />
+      <Provider store={store}>
+        <Router />
+      </Provider>
     </ChakraProvider>
   </React.StrictMode>,
 );
