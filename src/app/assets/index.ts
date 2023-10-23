a0129e8d2b3fd5b243832f7cd5ae4d2cf33d9341
jpcchaves <joao.chaves@liferay.com>
2023-10-23 16:15:52 -0300

    feat: add 404 animation

--- src/app/assets/index.ts
+++ src/app/assets/index.ts
@@ -1,5 +1,6 @@
 import authAnimation from "./animation/animation_lnwgxd9m.json";
+import animation404 from "./animation/404-animation.json";
 
 import logo from "./logo.png";
 
-export { logo, authAnimation };
+export { logo, authAnimation, animation404 };
