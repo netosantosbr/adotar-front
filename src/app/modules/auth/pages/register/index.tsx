d5cd6b3d95f1e39b518a72b6cc6e27f0d3ce270d
jpcchaves <joao.chaves@liferay.com>
2023-10-24 11:26:09 -0300

    feat: add base files for register page

---
+++ src/app/modules/auth/pages/register/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import RegisterView from "./view";
+
+interface IProps {}
+
+const Register: React.FC<IProps> = () => {
+  return <RegisterView />;
+};
+
+export default Register;
---
+++ src/app/modules/auth/pages/register/view.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface IProps {}
+
+const RegisterView: React.FC<IProps> = () => {
+  return <div></div>;
+};
+
+export default RegisterView;
