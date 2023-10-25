63833d67e73336c73fee44c2511a8c5359fe3689
jpcchaves <joao.chaves@liferay.com>
2023-10-25 09:21:34 -0300

    style: add new input variant to support floating label

--- src/app/theme/styles.ts
+++ src/app/theme/styles.ts
@@ -1,5 +1,9 @@
 import { mode } from "@chakra-ui/theme-tools";
 
+const activeLabelStyles = {
+  transform: "scale(0.85) translateY(-24px)",
+};
+
 export const globalStyles = {
   colors: {
     gray: {
@@ -30,5 +34,39 @@ export const globalStyles = {
         fontFamily: "Helvetica, sans-serif",
       },
     }),
+  },
+  components: {
+    Form: {
+      variants: {
+        floating: {
+          container: {
+            _focusWithin: {
+              label: {
+                ...activeLabelStyles,
+              },
+            },
+            "input:not(:placeholder-shown) + label, .chakra-select__wrapper + label, textarea:not(:placeholder-shown) ~ label":
+              {
+                ...activeLabelStyles,
+              },
+            label: {
+              top: 0,
+              left: 0,
+              zIndex: 2,
+              position: "absolute",
+              backgroundColor: "gray.50",
+              pointerEvents: "none",
+              mx: 3,
+              px: 1,
+              my: 2,
+              transformOrigin: "left top",
+              _dark: {
+                backgroundColor: "#1B254B", // Set the dark mode background color for the label
+              },
+            },
+          },
+        },
+      },
+    },
   },
 };
